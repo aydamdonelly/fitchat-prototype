fd1b82854a4adfbe20deab9eed5bd7878328c4e9
Yves Zumbuehl <yves.zumbuehl@gmail.com>
2024-07-05 17:50:34 +0200

    begin work with new user model and data structure

--- app/models.py
+++ app/models.py
@@ -1,14 +1,30 @@
 from langchain_core.pydantic_v1 import BaseModel, Field
 from typing import Optional
-from datetime import date
+from datetime import datetime
+from langchain_core.messages import BaseMessage
 
-class DailyProgress(BaseModel):
-    # date of the progreess (automatically set to today using datetime)
-    date: str = Field(default_factory=lambda: datetime.now().strftime('%Y-%m-%d'))
-    calories: Optional[int] = Field(description="calories consumed today")
-    steps: Optional[int] = Field(description="steps taken today")
-    weight: Optional[float] = Field(description="weight in kg")
-    height: Optional[int] = Field(description="height in cm")
+
+class ActivityLog(BaseModel):
+    calories: Optional[int] = Field(description="Total calories consumed today")
+    steps: Optional[int] = Field(description="Number of steps taken today")
+    weight: Optional[float] = Field(description="Current body weight in kilograms")
+    protein: Optional[float] = Field(description="Total grams of protein consumed today")
+    
+class DatedActivityLog(ActivityLog):
+    date: str = Field(default_factory=lambda: datetime.now().strftime('%Y-%m-%d'), description="Date of the activity log in YYYY-MM-DD format")
+    
+class UserData(BaseModel):
+    first_name: Optional[str] = Field(description="name of the user")
+    age: Optional[str] = Field(description="age of the user")
     gender: Optional[str] = Field(description="gender of the user")
-    age: Optional[int] = Field(description="age of the user")
-    protein: Optional[float] = Field(description="grams of protein consumed today")
+    height: Optional[int] = Field(description="height in cm")
+    
+class Message(BaseModel):
+    time: Optional[datetime] = Field(default_factory=datetime.now, description="time of the message")
+    base_message: BaseMessage = Field(description="message")
+        
+class User(BaseModel):
+    paid: bool = Field(default=False, description="paid user")
+    user_data: UserData = Field(default_factory=UserData, description="user data")
+    activity_log: Optional[DatedActivityLog] = Field(description="activity log")
+    messages: list[Message] = Field(default_factory=list, description="messages")
